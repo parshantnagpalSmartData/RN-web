f058feacb9079b24f8685b8373c4b9cb41e5bc39
Parshant Nagpal <parshant.nagpal@isteams-Mac-mini-98.local>
2018-12-12 14:23:21 +0530

    web routing

--- index.web.js
+++ index.web.js
@@ -1,18 +1,64 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import AppData from './src';
 import setup from "./src/store/setup";
 const store = setup()
+const Home = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Topic = ({ match }) => <h3>Requested Param: {match.params.id}</h3>;
+const Topics = ({ match }) => (
+  <div>
+    <h2>Topics</h2>
+
+    <ul>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route path={`${match.path}/:id`} component={Topic} />
+    <Route
+      exact
+      path={match.path}
+      render={() => <h3>Please select a topic.</h3>}
+    />
+  </div>
+);
+
+
+const Header = () => (
+  <ul>
+    <li>
+      <Link to="/">Home</Link>
+    </li>
+    <li>
+      <Link to="/about">About</Link>
+    </li>
+    <li>
+      <Link to="/topics">Topics</Link>
+    </li>
+  </ul>
+);
 class App extends React.Component {
   
   render() {
     return (
       <Provider store={store}>
-      <View style={styles.box}>
-        <Text style={styles.text}>Hello, world!</Text>
-        <AppData/>  
-      </View>
+      <Router>
+    <div>
+      <Header />
+
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/topics" component={Topics} />
+    </div>
+  </Router>
       </Provider>
     );
   }
