43500118909a356b0130d4aa8253b8b128784e6d
Suraj Sanwal <suraj.sanwal@smartdatainc.net>
2019-01-15 19:04:34 +0530

    div container update

--- src/components/common/patientsDetails/PatientsDetails.js
+++ src/components/common/patientsDetails/PatientsDetails.js
@@ -11,6 +11,7 @@ import moment from "moment";
 import Constants from "../../../constants";
 import { moderateScale } from "../../../helpers/ResponsiveFonts";
 import PatientInfo from "./patientInfo";
+import DivContainer from "../DivContainer";
 
 const PatientsDetails = props => {
   let {
@@ -24,51 +25,53 @@ const PatientsDetails = props => {
     blankView
   } = props;
   return (
-    <View style={Styles.container}>
-      {birthday ? (
-        <PatientInfo
-          image={Constants.Images.Age}
-          heading={"Age"}
-          value={`${moment().diff(birthday, "years")} yrs`}
-        />
-      ) : null}
-      {date ? (
-        <PatientInfo
-          image={Constants.Images.Calander}
-          heading={"Date"}
-          value={`${moment(date).format("MM-DD-YYYY ddd")}`}
-        />
-      ) : null}
-      {StartTime && EndTime ? (
-        <PatientInfo
-          image={Constants.Images.Date}
-          heading={"Time"}
-          value={`${StartTime}-${EndTime}`}
-        />
-      ) : null}
-      {age ? (
-        <PatientInfo
-          image={Constants.Images.Age}
-          heading={"Age"}
-          value={`${age} yrs`}
-        />
-      ) : null}
-      {gender ? (
-        <PatientInfo
-          image={Constants.Images.Gender}
-          heading={"Patient Gender"}
-          value={gender}
-        />
-      ) : null}
-      {zip ? (
-        <PatientInfo
-          image={Constants.Images.ZipCode}
-          heading={"Zipcode"}
-          value={zip}
-        />
-      ) : null}
-      {blankView ? <PatientInfo image={""} heading={""} value={""} /> : null}
-    </View>
+    <DivContainer>
+      <View style={Styles.container}>
+        {birthday ? (
+          <PatientInfo
+            image={Constants.Images.Age}
+            heading={"Age"}
+            value={`${moment().diff(birthday, "years")} yrs`}
+          />
+        ) : null}
+        {date ? (
+          <PatientInfo
+            image={Constants.Images.Calander}
+            heading={"Date"}
+            value={`${moment(date).format("MM-DD-YYYY ddd")}`}
+          />
+        ) : null}
+        {StartTime && EndTime ? (
+          <PatientInfo
+            image={Constants.Images.Date}
+            heading={"Time"}
+            value={`${StartTime}-${EndTime}`}
+          />
+        ) : null}
+        {age ? (
+          <PatientInfo
+            image={Constants.Images.Age}
+            heading={"Age"}
+            value={`${age} yrs`}
+          />
+        ) : null}
+        {gender ? (
+          <PatientInfo
+            image={Constants.Images.Gender}
+            heading={"Patient Gender"}
+            value={gender}
+          />
+        ) : null}
+        {zip ? (
+          <PatientInfo
+            image={Constants.Images.ZipCode}
+            heading={"Zipcode"}
+            value={zip}
+          />
+        ) : null}
+        {blankView ? <PatientInfo image={""} heading={""} value={""} /> : null}
+      </View>
+    </DivContainer>
   );
 };
 
