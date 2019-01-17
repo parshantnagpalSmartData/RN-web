18f8409af36f755ec94249e144308835e80e1097
Suraj Sanwal <suraj.sanwal@smartdatainc.net>
2019-01-17 11:45:52 +0530

    divContainer merge

--- src/components/printableForms/index.js
+++ src/components/printableForms/index.js
@@ -1,5 +1,39 @@
-import RenderPrintableForms from "./RenderPrintableForms";
-import PDFViewer from "./PDFViewer";
+/**
+ * Name : Parshant Nagpal
+ * FileName: PrintableForms
+ * Description : Contains the pdf rendering part
+ */
+import React from "react";
+import { StyleSheet } from "react-native";
+import Pdf from "react-native-pdf";
+import Constants from "../../constants";
 
-export { PDFViewer };
-export default RenderPrintableForms;
+const source = {
+  uri: "http://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf",
+  cache: true
+};
+
+const PrintableForms = () => {
+  return (
+    <Pdf
+      source={source} // eslint-disable-next-line
+      onLoadComplete={(numberOfPages, filePath) => {
+        console.log(`number of pages: ${numberOfPages}`); // eslint-disable-line
+      }} // eslint-disable-next-line
+      onPageChanged={(page, numberOfPages) => {
+        // eslint-disable-line
+        console.log(`current page: ${page}`); // eslint-disable-line
+      }} // eslint-disable-next-line
+      onError={error => {}}
+      style={Styles.pdf}
+    />
+  );
+};
+export default PrintableForms;
+
+const Styles = StyleSheet.create({
+  pdf: {
+    flex: 1,
+    width: Constants.BaseStyle.DEVICE_WIDTH
+  }
+});
