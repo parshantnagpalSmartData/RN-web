96b09cda5ddd59491bd0bef9734cebc6d5537121
Parshant <parshant.nagpal@smartdatainc.net>
2019-01-25 18:47:44 +0530

    pdf implementation and resources

--- src/actions/app/commonAppActions.js
+++ src/actions/app/commonAppActions.js
@@ -21,11 +21,6 @@ export const stopLoader = () => {
   };
 };
 
-export const stopLoader2 = () => {
-  return dispatch => {
-    dispatch({ type: Types.STOP_LOADER });
-  };
-};
 export const startRefreshLoader = () => {
   return dispatch => {
     dispatch({ type: Types.START_REFRESH_LOADER });
