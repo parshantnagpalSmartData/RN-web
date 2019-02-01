e6e3d6e6b27586216a00c876508aa067111cb309
Parshant <parshant.nagpal@smartdatainc.net>
2019-02-01 19:56:45 +0530

    swipe callback handle

--- src/components/MessageCenter/SwiperContainer.js
+++ src/components/MessageCenter/SwiperContainer.js
@@ -56,7 +56,7 @@ const SwiperContainer = ({
               onPressIsChecked={() => onPressIsChecked(index)}
               item={item}
               index={index}
-              onDeletePress={(cb) => onDeletePress(item, cb)}
+              onDeletePress={cb => onDeletePress(item, cb)}
               MessageSubject={item.MessageSubject}
               onMessagePress={onMessagePress}
             />
