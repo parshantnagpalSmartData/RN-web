65d23b3f6551f03afd14f6b7ce106def4ca645de
Parshant <parshant.nagpal@smartdatainc.net>
2019-02-08 15:33:13 +0530

    Message center design fixing

--- src/container/MessageCenter/MessageDetails/index.js
+++ src/container/MessageCenter/MessageDetails/index.js
@@ -8,17 +8,17 @@ import React, { Component } from "react";
 import { View, StyleSheet } from "react-native";
 import MessageDetailsComp from "./MessageDetails";
 class MessageDetails extends Component {
-    render() {
-        return (
-            <View style={Styles.container}>
-                <MessageDetailsComp {...this.props} />
-            </View>
-        );
-    }
+  render() {
+    return (
+      <View style={Styles.container}>
+        <MessageDetailsComp {...this.props} />
+      </View>
+    );
+  }
 }
 const Styles = StyleSheet.create({
-    container: {
-        flex: 1
-    }
+  container: {
+    flex: 1
+  }
 });
 export default MessageDetails;
